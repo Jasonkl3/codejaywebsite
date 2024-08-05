ded59e066c880c257341d49b3586cb7ed0b100c6
Jason Llanos <jasonllanos@Jasons-MBP.attlocal.net>
2024-08-04 17:34:24 -0700

    add google analytics

---
+++ src/app/GoogleAnalytics.tsx
@@ -0,0 +1,39 @@
+// GoogleAnalytics.tsx
+
+import React from 'react';
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      {/* <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+      />
+
+      <Script id='' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script> */}
+      {/* <!-- Google tag (gtag.js) --> */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-5J119TH9NV"></Script>
+        <Script id='' strategy='lazyOnload'>
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-5J119TH9NV');
+            `}
+        </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
